19fa6cb6de732f7a5d084073f665581840bda9db
Nicolas A. Barriga <nbarriga@gmail.com>
2015-03-09 17:02:47 -0600

    Added C style function to Logger

--- UAlbertaBot/Source/Logger.cpp
+++ UAlbertaBot/Source/Logger.cpp
@@ -1,4 +1,6 @@
 #include "Logger.h"
+#include <stdarg.h>
+#include <cstdio>
 
 namespace UAlbertaBot
 {
@@ -12,6 +14,23 @@ namespace Logger
         logStream.flush();
         logStream.close();
     }
+
+	void LogAppendToFile(const std::string & logFile, const char *fmt, ...)
+	{
+		va_list arg;
+		
+		va_start(arg, fmt);
+		//vfprintf(log_file, fmt, arg);
+		char buff[256];
+		vsnprintf_s(buff, 256, fmt, arg);
+		va_end(arg);
+		
+		std::ofstream logStream;
+		logStream.open(logFile.c_str(), std::ofstream::app);
+		logStream << buff;
+		logStream.flush();
+		logStream.close();
+	}
 
     void LogOverwriteToFile(const std::string & logFile, const std::string & msg)
     {
