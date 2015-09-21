e002ac86b845a27030cbaba326840b7cc8a048d1
David Churchill <dave.churchill@gmail.com>
2015-09-20 19:38:35 -0600

    format micro

--- UAlbertaBot/Source/Micro.cpp
+++ UAlbertaBot/Source/Micro.cpp
@@ -15,7 +15,7 @@ void Micro::drawAPM(int x, int y)
 
 void Micro::SmartAttackUnit(BWAPI::UnitInterface* attacker, BWAPI::UnitInterface* target)
 {
-	UAB_ASSERT(attacker, "SmartAttackUnit: Attacker not valid");
+    UAB_ASSERT(attacker, "SmartAttackUnit: Attacker not valid");
     UAB_ASSERT(target, "SmartAttackUnit: Target not valid");
 
     if (!attacker || !target)
@@ -23,37 +23,36 @@ void Micro::SmartAttackUnit(BWAPI::UnitInterface* attacker, BWAPI::UnitInterface
         return;
     }
 
-	// if we have issued a command to this unit already this frame, ignore this one
-	if (attacker->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || attacker->isAttackFrame())
-	{
-		return;
-	}
+    // if we have issued a command to this unit already this frame, ignore this one
+    if (attacker->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || attacker->isAttackFrame())
+    {
+        return;
+    }
 
-	// get the unit's current command
-	BWAPI::UnitCommand currentCommand(attacker->getLastCommand());
+    // get the unit's current command
+    BWAPI::UnitCommand currentCommand(attacker->getLastCommand());
 
-	// if we've already told this unit to attack this target, ignore this command
-	if (currentCommand.getType() == BWAPI::UnitCommandTypes::Attack_Unit &&	currentCommand.getTarget() == target)
-	{
-		return;
-	}
+    // if we've already told this unit to attack this target, ignore this command
+    if (currentCommand.getType() == BWAPI::UnitCommandTypes::Attack_Unit &&	currentCommand.getTarget() == target)
+    {
+        return;
+    }
 
-	// if nothing prevents it, attack the target
-	attacker->attack(target);
+    // if nothing prevents it, attack the target
+    attacker->attack(target);
     TotalCommands++;
 
-	if (Config::Debug::DrawUnitTargetInfo) 
+    if (Config::Debug::DrawUnitTargetInfo) 
     {
         BWAPI::Broodwar->drawCircleMap(attacker->getPosition(), dotRadius, BWAPI::Colors::Red, true);
         BWAPI::Broodwar->drawCircleMap(target->getPosition(), dotRadius, BWAPI::Colors::Red, true);
         BWAPI::Broodwar->drawLineMap( attacker->getPosition(), target->getPosition(), BWAPI::Colors::Red );
     }
-
 }
 
 void Micro::SmartAttackMove(BWAPI::UnitInterface* attacker, const BWAPI::Position & targetPosition)
 {
-	UAB_ASSERT(attacker, "SmartAttackMove: Attacker not valid");
+    UAB_ASSERT(attacker, "SmartAttackMove: Attacker not valid");
     UAB_ASSERT(targetPosition.isValid(), "SmartAttackMove: targetPosition not valid");
 
     if (!attacker || !targetPosition.isValid())
@@ -61,23 +60,23 @@ void Micro::SmartAttackMove(BWAPI::UnitInterface* attacker, const BWAPI::Positio
         return;
     }
 
-	// if we have issued a command to this unit already this frame, ignore this one
-	if (attacker->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || attacker->isAttackFrame())
+    // if we have issued a command to this unit already this frame, ignore this one
+    if (attacker->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || attacker->isAttackFrame())
+    {
+        return;
+    }
+
+    // get the unit's current command
+    BWAPI::UnitCommand currentCommand(attacker->getLastCommand());
+
+    // if we've already told this unit to attack this target, ignore this command
+    if (currentCommand.getType() == BWAPI::UnitCommandTypes::Attack_Move &&	currentCommand.getTargetPosition() == targetPosition)
 	{
 		return;
 	}
 
-	// get the unit's current command
-	BWAPI::UnitCommand currentCommand(attacker->getLastCommand());
-
-	// if we've already told this unit to attack this target, ignore this command
-	if (currentCommand.getType() == BWAPI::UnitCommandTypes::Attack_Move &&	currentCommand.getTargetPosition() == targetPosition)
-	{
-		return;
-	}
-
-	// if nothing prevents it, attack the target
-	attacker->attack(targetPosition);
+    // if nothing prevents it, attack the target
+    attacker->attack(targetPosition);
     TotalCommands++;
 
     if (Config::Debug::DrawUnitTargetInfo) 
@@ -90,7 +89,7 @@ void Micro::SmartAttackMove(BWAPI::UnitInterface* attacker, const BWAPI::Positio
 
 void Micro::SmartMove(BWAPI::UnitInterface* attacker, const BWAPI::Position & targetPosition)
 {
-	UAB_ASSERT(attacker, "SmartAttackMove: Attacker not valid");
+    UAB_ASSERT(attacker, "SmartAttackMove: Attacker not valid");
     UAB_ASSERT(targetPosition.isValid(), "SmartAttackMove: targetPosition not valid");
 
     if (!attacker || !targetPosition.isValid())
@@ -98,26 +97,26 @@ void Micro::SmartMove(BWAPI::UnitInterface* attacker, const BWAPI::Position & ta
         return;
     }
 
-	// if we have issued a command to this unit already this frame, ignore this one
-	if (attacker->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || attacker->isAttackFrame())
-	{
-		return;
-	}
-
-	// get the unit's current command
-	BWAPI::UnitCommand currentCommand(attacker->getLastCommand());
-
-	// if we've already told this unit to move to this position, ignore this command
-	if ((currentCommand.getType() == BWAPI::UnitCommandTypes::Move) && (currentCommand.getTargetPosition() == targetPosition) && attacker->isMoving())
+    // if we have issued a command to this unit already this frame, ignore this one
+    if (attacker->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || attacker->isAttackFrame())
     {
-		return;
-	}
+        return;
+    }
 
-	// if nothing prevents it, attack the target
-	attacker->move(targetPosition);
+    // get the unit's current command
+    BWAPI::UnitCommand currentCommand(attacker->getLastCommand());
+
+    // if we've already told this unit to move to this position, ignore this command
+    if ((currentCommand.getType() == BWAPI::UnitCommandTypes::Move) && (currentCommand.getTargetPosition() == targetPosition) && attacker->isMoving())
+    {
+        return;
+    }
+
+    // if nothing prevents it, attack the target
+    attacker->move(targetPosition);
     TotalCommands++;
 
-	if (Config::Debug::DrawUnitTargetInfo) 
+    if (Config::Debug::DrawUnitTargetInfo) 
     {
         BWAPI::Broodwar->drawCircleMap(attacker->getPosition(), dotRadius, BWAPI::Colors::White, true);
         BWAPI::Broodwar->drawCircleMap(targetPosition, dotRadius, BWAPI::Colors::White, true);
@@ -127,7 +126,7 @@ void Micro::SmartMove(BWAPI::UnitInterface* attacker, const BWAPI::Position & ta
 
 void Micro::SmartRightClick(BWAPI::UnitInterface * unit, BWAPI::UnitInterface * target)
 {
-	UAB_ASSERT(unit, "SmartRightClick: Unit not valid");
+    UAB_ASSERT(unit, "SmartRightClick: Unit not valid");
     UAB_ASSERT(target, "SmartRightClick: Target not valid");
 
     if (!unit || !target)
@@ -135,26 +134,26 @@ void Micro::SmartRightClick(BWAPI::UnitInterface * unit, BWAPI::UnitInterface * 
         return;
     }
 
-	// if we have issued a command to this unit already this frame, ignore this one
-	if (unit->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || unit->isAttackFrame())
-	{
-		return;
-	}
+    // if we have issued a command to this unit already this frame, ignore this one
+    if (unit->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || unit->isAttackFrame())
+    {
+        return;
+    }
 
-	// get the unit's current command
-	BWAPI::UnitCommand currentCommand(unit->getLastCommand());
+    // get the unit's current command
+    BWAPI::UnitCommand currentCommand(unit->getLastCommand());
 
-	// if we've already told this unit to move to this position, ignore this command
-	if ((currentCommand.getType() == BWAPI::UnitCommandTypes::Right_Click_Unit) && (currentCommand.getTargetPosition() == target->getPosition()))
-	{
-		return;
-	}
+    // if we've already told this unit to move to this position, ignore this command
+    if ((currentCommand.getType() == BWAPI::UnitCommandTypes::Right_Click_Unit) && (currentCommand.getTargetPosition() == target->getPosition()))
+    {
+        return;
+    }
 
-	// if nothing prevents it, attack the target
-	unit->rightClick(target);
+    // if nothing prevents it, attack the target
+    unit->rightClick(target);
     TotalCommands++;
 
-	if (Config::Debug::DrawUnitTargetInfo) 
+    if (Config::Debug::DrawUnitTargetInfo) 
     {
         BWAPI::Broodwar->drawCircleMap(unit->getPosition(), dotRadius, BWAPI::Colors::Cyan, true);
         BWAPI::Broodwar->drawCircleMap(target->getPosition(), dotRadius, BWAPI::Colors::Cyan, true);
@@ -164,7 +163,7 @@ void Micro::SmartRightClick(BWAPI::UnitInterface * unit, BWAPI::UnitInterface * 
 
 void Micro::SmartRepair(BWAPI::UnitInterface * unit, BWAPI::UnitInterface * target)
 {
-    	UAB_ASSERT(unit, "SmartRightClick: Unit not valid");
+    UAB_ASSERT(unit, "SmartRightClick: Unit not valid");
     UAB_ASSERT(target, "SmartRightClick: Target not valid");
 
     if (!unit || !target)
@@ -172,26 +171,26 @@ void Micro::SmartRepair(BWAPI::UnitInterface * unit, BWAPI::UnitInterface * targ
         return;
     }
 
-	// if we have issued a command to this unit already this frame, ignore this one
-	if (unit->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || unit->isAttackFrame())
-	{
-		return;
-	}
+    // if we have issued a command to this unit already this frame, ignore this one
+    if (unit->getLastCommandFrame() >= BWAPI::Broodwar->getFrameCount() || unit->isAttackFrame())
+    {
+        return;
+    }
 
-	// get the unit's current command
-	BWAPI::UnitCommand currentCommand(unit->getLastCommand());
+    // get the unit's current command
+    BWAPI::UnitCommand currentCommand(unit->getLastCommand());
 
-	// if we've already told this unit to move to this position, ignore this command
-	if ((currentCommand.getType() == BWAPI::UnitCommandTypes::Repair) && (currentCommand.getTarget() == target))
-	{
-		return;
-	}
+    // if we've already told this unit to move to this position, ignore this command
+    if ((currentCommand.getType() == BWAPI::UnitCommandTypes::Repair) && (currentCommand.getTarget() == target))
+    {
+        return;
+    }
 
-	// if nothing prevents it, attack the target
-	unit->repair(target);
+    // if nothing prevents it, attack the target
+    unit->repair(target);
     TotalCommands++;
 
-	if (Config::Debug::DrawUnitTargetInfo) 
+    if (Config::Debug::DrawUnitTargetInfo) 
     {
         BWAPI::Broodwar->drawCircleMap(unit->getPosition(), dotRadius, BWAPI::Colors::Cyan, true);
         BWAPI::Broodwar->drawCircleMap(target->getPosition(), dotRadius, BWAPI::Colors::Cyan, true);
