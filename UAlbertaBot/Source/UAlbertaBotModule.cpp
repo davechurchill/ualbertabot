dc38aaff56d0f5ed517ae0da1a6d815fe79cdb67
Dennis <dtruong1@ualberta.ca>
2015-11-08 02:05:17 -0700

    speed

--- UAlbertaBot/Source/UAlbertaBotModule.cpp
+++ UAlbertaBot/Source/UAlbertaBotModule.cpp
@@ -27,7 +27,7 @@ void UAlbertaBotModule::onStart()
 
     // Parse the bot's configuration file if it has one, change this file path to where your config file is
     // Any relative path name will be relative to Starcraft installation folder
-    parseConfigFile("D:/ualbertabot/UAlbertaBot/UAlbertaBot_Config.txt");
+    parseConfigFile("C:/ualbertabot/UAlbertaBot/UAlbertaBot_Config.txt");
 
     // Set our BWAPI options here    
 	BWAPI::Broodwar->setLocalSpeed(Config::BWAPIOptions::SetLocalSpeed);
