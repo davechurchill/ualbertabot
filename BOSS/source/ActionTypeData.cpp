3c35602573bf84d905891991606c76d80edec1c9
varsis <chris@varsisstudio.com>
2015-11-28 12:19:42 -0700

    oops edited wrong file

--- BOSS/source/ActionTypeData.cpp
+++ BOSS/source/ActionTypeData.cpp
@@ -545,7 +545,8 @@ PrerequisiteSet ActionTypeData::CalculatePrerequisites(const ActionTypeData & ac
 	{
 		if (action.getUpgradeType().whatUpgrades() != BWAPI::UnitTypes::None)
 		{
-			//if (DEBUG_StarcraftData) printf("\tPRE: %s\n", action.getUpgradeType().whatUpgrades().getName().c_str());
+			//if (DEBUG_StarcraftData) 
+			BWAPI::Broodwar->printf("\tPRE: %s\n", action.getUpgradeType().whatUpgrades().getName().c_str());
 
 			// add what upgrades it
 			pre.add(ActionType(action.getRaceID(), GetActionID(action.getUpgradeType().whatUpgrades())), 1);
