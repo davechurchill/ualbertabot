50bff938b1f3897084360c947971ad1eb90e132f
David Churchill <dave.churchill@gmail.com>
2016-11-02 00:54:48 -0230

    updated frame visualization

--- SparCraft/source/main/main_statevis.cpp
+++ SparCraft/source/main/main_statevis.cpp
@@ -9,11 +9,10 @@ using namespace cimg_library;
 #undef max
 
 using namespace SparCraft;
-unsigned char White[3] = {255, 255, 255};
-unsigned char Grid[3] = {25, 25, 25};
-unsigned char PlayerColors[2][3] = {{0, 255, 0}, {255, 0, 0}};
 
-int scale = 2;
+const unsigned char White[3] = {255, 255, 255};
+const unsigned char GridColor[3] = {25, 25, 25};
+const unsigned char PlayerColors[2][3] = {{0, 255, 0}, {255, 0, 0}};
 
 void DrawLine(CImg<unsigned char> & img, const Position & p1, const Position & p2, const float thickness, const unsigned char *const color)
 {
@@ -33,7 +32,7 @@ void DrawTorchUnit(CImg<unsigned char> & img, const replayer::Frame & frame, con
     const BWAPI::UnitType type(unit.type);
     Position pos(unit.x * 8, unit.y * 8);
 
-    std::cout << playerID << " "  << unit.id << " " << type.getName();
+    //std::cout << playerID << " "  << unit.id << " " << type.getName();
 
     Position tl(pos.x() - type.dimensionLeft(),  pos.y() - type.dimensionUp());
     Position br(pos.x() + type.dimensionRight(), pos.y() + type.dimensionDown());
@@ -54,13 +53,10 @@ void DrawTorchUnit(CImg<unsigned char> & img, const replayer::Frame & frame, con
     img.draw_text(tl.x(), tl.y()-12, ss.str().c_str(), White);
     img.draw_text(tl.x()+2, tl.y(), "%d", White, 0, 1, 8, unit.health);
 
-    std::cout << " [";
-    for (const replayer::Order & order : unit.orders)
+    if (unit.orders.size() > 0)
     {
-        std::cout << order.targetId << " ";
-
+        const replayer::Order & order = unit.orders.back();
         BWAPI::Order bwapiOrder(order.type);
-        std::cout << bwapiOrder.getName() << " ";
 
         if (bwapiOrder == BWAPI::Orders::AttackUnit)
         {
@@ -82,7 +78,6 @@ void DrawTorchUnit(CImg<unsigned char> & img, const replayer::Frame & frame, con
 
         DrawLine(img, pos, Position(order.targetX * 8, order.targetY * 8), 1, White);
     }
-    std::cout << "]\n";
 }
 
 void DrawTorchFrame(CImg<unsigned char> & img, int width, int height, const replayer::Frame & frame)
@@ -91,12 +86,12 @@ void DrawTorchFrame(CImg<unsigned char> & img, int width, int height, const repl
 
     for (int x(32); x < width; x += 32)
     {
-        DrawLine(img, Position(x, 0), Position(x, height), 1, Grid);
+        DrawLine(img, Position(x, 0), Position(x, height), 1, GridColor);
     }
 
     for (int y(32); y < height; y += 32)
     {
-        DrawLine(img, Position(0, y), Position(width, y), 1, Grid);
+        DrawLine(img, Position(0, y), Position(width, y), 1, GridColor);
     }
 
 	for (const auto & kv : allUnits)
@@ -114,35 +109,103 @@ void DrawTorchFrame(CImg<unsigned char> & img, int width, int height, const repl
 int main(int argc, char ** argv)
 {
     std::string file = "frames.txt";  file = argv[1];
-    int mapWidth = 2000;    mapWidth = atoi(argv[2]);
-    int mapHeight = 2000;   mapHeight = atoi(argv[3]);
-    int mapTLX = 0;         mapTLX = atoi(argv[4]);
-    int mapTLY = 0;         mapTLY = atoi(argv[5]);
-    int mapBRX = mapWidth;  mapBRX = atoi(argv[6]);
-    int mapBRY = mapHeight; mapBRY = atoi(argv[7]);
-    int scale = 1;          scale = atoi(argv[8]);
+    int mapWidth = 2000, mapHeight = 2000;   
+    int mapTLX = 0, mapTLY = 0;         
+    int mapBRX = mapWidth, mapBRY = mapHeight; 
+    int finalScalingFactor = 2;
+    int padding = 100;
 
+    // Check to see if the file exists
     std::ifstream fin(file);
+    if (!fin.good())
+    {
+        std::cerr << "Error: Could not read file: " << file << "\n";
+        exit(-1);
+    }
 
-    int lineNum = 0;
+    // Read in all of the Frame data from the file
+    std::vector<replayer::Frame> frames;
     while (fin.good())
     {
         replayer::Frame frame;
 	    fin >> frame;
+        frames.push_back(frame);
+    }
 
+    // Check to see if any valid frames were found
+    if (frames.empty())
+    {
+        std::cerr << "Error: No valid frames found in file\n";
+        exit(-1);
+    }
+
+    // Set up the image dimensions from args if there are enough
+    if (argc == 9)
+    {
+        mapWidth = atoi(argv[2]);
+        mapHeight = atoi(argv[3]);
+        mapTLX = atoi(argv[4]);
+        mapTLY = atoi(argv[5]);
+        mapBRX = atoi(argv[6]);
+        mapBRY = atoi(argv[7]);
+        finalScalingFactor = atoi(argv[8]);
+    }
+    // Otherwise, set up default image dimensions
+    else
+    {
+        if (argc == 3)
+        {
+            finalScalingFactor = atoi(argv[2]);
+        }
+
+        int minX = std::numeric_limits<int>::max();
+        int minY = std::numeric_limits<int>::max();
+        int maxX = std::numeric_limits<int>::min();
+        int maxY = std::numeric_limits<int>::min();
+
+        // Determine the dimensions of the image from the frame data
+        for (size_t f(0); f < frames.size(); ++f)
+        {
+            const auto & allUnits = frames[f].units;
+            for (const auto & kv : allUnits)
+	        {
+		        const auto & playerUnits = kv.second;
+		        for (const auto & unit : playerUnits)
+		        {
+                    BWAPI::UnitType type(unit.type);
+
+                    minX = std::min(minX, unit.x*8 - type.dimensionLeft());
+                    minY = std::min(minY, unit.y*8 - type.dimensionUp());
+                    maxX = std::max(maxX, unit.x*8 + type.dimensionRight());
+                    maxY = std::max(maxY, unit.y*8 + type.dimensionDown());
+		        }
+	        }
+        }
+
+        mapWidth = maxX + padding;
+        mapHeight = maxY + padding;
+        mapTLX = std::max(0, minX - padding);
+        mapTLY = std::max(0, minY - padding);
+        mapBRX = mapWidth;
+        mapBRY = mapHeight;
+
+        std::cout << mapWidth << " " << mapHeight << "\n";
+    }
+
+    // Draw the images from the frame data
+    for (size_t f(0); f < frames.size(); ++f)
+    {
         CImg<unsigned char> canvas(mapWidth, mapHeight, 1, 4);
         canvas.fill(0);
 
-        DrawTorchFrame(canvas, mapWidth, mapHeight, frame);
+        DrawTorchFrame(canvas, mapWidth, mapHeight, frames[f]);
         canvas.crop(mapTLX, mapTLY, mapBRX, mapBRY);
-        canvas.resize(canvas.width() * scale, canvas.height() * scale, -1);
+        canvas.resize(canvas.width() * finalScalingFactor, canvas.height() * finalScalingFactor, -1);
 
         std::stringstream name;
-        name << "test_" << lineNum << ".png";
+        name << file << "_frame_" << f << ".png";
         canvas.save(name.str().c_str());
-
-        lineNum++;
-    }
+    }  
 
     return 0;
 }
