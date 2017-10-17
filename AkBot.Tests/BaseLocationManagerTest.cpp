8ea5438f45cd34e7199238a06994cc5bbe2264c5
Andrey Kyrdyumov <kant2002@gmail.com>
2017-10-18 02:58:02 +0300

    Make sure that resource clusters does not differentiate between minerals and geysers

--- AkBot.Tests/BaseLocationManagerTest.cpp
+++ AkBot.Tests/BaseLocationManagerTest.cpp
@@ -57,7 +57,14 @@ void placeMineral(BWAPI::UnitData& unitData, int x, int y, int resources)
 	unitData.positionX = x;
 	unitData.positionY = y;
 	unitData.type = BWAPI::UnitTypes::Resource_Mineral_Field;
-	unitData.buildType = BWAPI::UnitTypes::Resource_Mineral_Field;
+	unitData.exists = true;
+	unitData.resources = resources;
+}
+void placeGeyser(BWAPI::UnitData& unitData, int x, int y, int resources)
+{
+	unitData.positionX = x;
+	unitData.positionY = y;
+	unitData.type = BWAPI::UnitTypes::Resource_Vespene_Geyser;
 	unitData.exists = true;
 	unitData.resources = resources;
 }
@@ -71,6 +78,18 @@ void discoverMineral(BWAPI::GameData& gameData, int x, int y, int resources)
 	auto& mineral = gameData.units[gameData.initialUnitCount];
 	mineral.player = gameData.neutral;
 	placeMineral(mineral, x, y, resources);
+	gameData.initialUnitCount++;
+}
+void discoverGeyser(BWAPI::GameData& gameData, int x, int y, int resources)
+{
+	auto& lastEvent = gameData.events[gameData.eventCount];
+	gameData.eventCount++;
+	lastEvent.type = BWAPI::EventType::Enum::UnitDiscover;
+	lastEvent.v1 = gameData.initialUnitCount;
+
+	auto& geyser = gameData.units[gameData.initialUnitCount];
+	geyser.player = gameData.neutral;
+	placeGeyser(geyser, x, y, resources);
 	gameData.initialUnitCount++;
 }
 void setupEmptyMap(BWAPI::GameData& gameData, int width, int height)
@@ -123,10 +142,7 @@ namespace AkBotTests
 			Assert::AreEqual(0U, baseLocations.size(), L"The base locations list should be empty");
 		}
 
-		BEGIN_TEST_METHOD_ATTRIBUTE(ClustersWith4MineralsCounted)
-			TEST_METHOD_ATTRIBUTE(L"TestCategory", L"WIP")
-			END_TEST_METHOD_ATTRIBUTE()
-			TEST_METHOD(ClustersWith4MineralsCounted)
+		TEST_METHOD(ClustersWith4MineralsCounted)
 		{
 			auto gameData = std::make_shared<BWAPI::GameData>();
 			auto rawGameData = gameData.get();
@@ -138,6 +154,35 @@ namespace AkBotTests
 			discoverMineral(*rawGameData, 2, 1, 200);
 			discoverMineral(*rawGameData, 3, 1, 200);
 			discoverMineral(*rawGameData, 4, 1, 200);
+			setupEmptyMap(*rawGameData, 100, 100);
+
+			auto gameImpl = std::make_shared<AKBot::GameImpl>(rawGameData);
+			auto game = gameImpl.get();
+			BWAPI::BroodwarPtr = game;
+			auto opponentView = std::make_shared<BWAPIOpponentView>(BWAPIOpponentView(game));
+			UAlbertaBot::BaseLocationManager manager(game, opponentView);
+			auto mapInformation = std::make_shared<BWAPIMapInformation>(game);
+			auto logger = std::make_shared<NullLogger>();
+			auto mapTools = std::make_shared<MapTools>(mapInformation, logger);
+			auto baseLocations = std::vector<UAlbertaBot::BaseLocation>();
+
+			game->onMatchStart();
+			manager.populateBaseLocations(mapTools, baseLocations);
+			Assert::AreEqual(1U, baseLocations.size(), L"The base locations list should be empty");
+		}
+
+		TEST_METHOD(ClustersCouldContainMineralsAndGeysers)
+		{
+			auto gameData = std::make_shared<BWAPI::GameData>();
+			auto rawGameData = gameData.get();
+			setP2PForces(rawGameData);
+			setPlayers(*rawGameData, 2);
+
+			// Create cluster of 4 minerals
+			discoverMineral(*rawGameData, 1, 1, 200);
+			discoverMineral(*rawGameData, 2, 1, 200);
+			discoverMineral(*rawGameData, 3, 1, 200);
+			discoverGeyser(*rawGameData, 4, 1, 200);
 			setupEmptyMap(*rawGameData, 100, 100);
 
 			auto gameImpl = std::make_shared<AKBot::GameImpl>(rawGameData);
