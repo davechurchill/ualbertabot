d69e770c59976a6ba4fbe667083d9dc509f5e616
Andrey Kyrdyumov <kant2002@gmail.com>
2018-03-05 02:32:47 +0200

    Add testing of the combat estimators

--- AKBot.CombatSimulation/main.cpp
+++ AKBot.CombatSimulation/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <experimental/filesystem>
 #include <simulation\FAPCombatEstimator.h>
 #include <simulation\SparCraftCombatEstimator.h>
+#include "BWAPIOpponentView.h"
 #include <GameHelper.h>
 #include <UnitHelper.h>
+#include <NullLogger.h>
 #include <BWAPI/Client/GameData.h>
 #include <BWAPI/Client/UnitData.h>
 #include <TestLib/GameImpl.h>
@@ -17,32 +21,68 @@ using std::cin;
 using std::endl;
 
 constexpr bool TOTAL_SUCCESS = true;
-constexpr bool TOTAL_FAILURE = true;
+constexpr bool TOTAL_FAILURE = false;
 
-void doTest(const std::string name, std::function<bool(CombatEstimator&)> worker, CombatEstimator& estimator, bool expected)
-{
-	cout << name << endl;
-	auto result = worker(estimator) == expected ? "SUCCESS" : "FAIL";
-	cout << "Result: " << result << endl;
-}
+int successTests = 0;
+int totalTests = 0;
 
-std::shared_ptr<AKBot::GameImpl> currentGame;
+BotMicroConfiguration configuration;
+BotSparCraftConfiguration sparcraftConfiguration;
 
-void setupFakeGame()
+std::shared_ptr<AKBot::GameImpl> setupFakeGame()
 {
 	auto gameData = std::make_shared<BWAPI::GameData>();
 	auto rawGameData = gameData.get();
 	setP2PForces(rawGameData);
 	setPlayers(*rawGameData, 2);
-	currentGame = std::make_shared<AKBot::GameImpl>(rawGameData);
+	rawGameData->enemy = 1;
+
+	auto currentGame = std::make_shared<AKBot::GameImpl>(rawGameData);
+	currentGame->onMatchStart();
+	return currentGame;
+}
+
+template<typename TCombatEstimator>
+void doTest(
+	const std::string name,
+	std::function<bool(CombatEstimator&)> worker,
+	std::function<TCombatEstimator(std::shared_ptr<AKBot::GameImpl>)> estimatorFactory,
+	bool expected)
+{
+	cout << name << endl;
+	auto currentGame = setupFakeGame();
 	auto game = currentGame.get();
 	BWAPI::BroodwarPtr = game;
+	auto estimator = estimatorFactory(currentGame);
+	auto success = worker(estimator) == expected;
+	BWAPI::BroodwarPtr = nullptr;
+	if (success)
+	{
+		successTests++;
+	}
+	
+	totalTests++;
+	auto result = success ? "SUCCESS" : "FAIL";
+	cout << "Result: " << result << endl;
+}
+
+void clearTestCounter()
+{
+	successTests = 0;
+	totalTests = 0;
+}
+void printStatistics()
+{
+	cout << std::fixed;
+	cout.precision(2);
+
+	cout << "Statistics: " << successTests << " out of " << totalTests << " tests passed" << endl;
+	auto successRate = 100.0 * successTests / totalTests;
+	cout << "Success rate: " << successRate << "%" << endl;
 }
 
 bool testZergling1(CombatEstimator& estimator)
 {
-	setupFakeGame();
-
 	BWAPI::UnitData unit;
 	placeZergZergeling(unit, 1, 1);
 	unit.player = 0;
@@ -57,8 +97,6 @@ bool testZergling1(CombatEstimator& estimator)
 
 bool testMarine1(CombatEstimator& estimator)
 {
-	setupFakeGame();
-
 	BWAPI::UnitData unit;
 	placeTerranMarine(unit, 1, 1);
 	unit.player = 0;
@@ -71,19 +109,68 @@ bool testMarine1(CombatEstimator& estimator)
 	return estimator.isWinPredicted(ourCombatUnits, enemyCombatUnits, 100);
 }
 
-void testCombatSimulation(std::string name, CombatEstimator& estimator)
+bool testMarine2(CombatEstimator& estimator)
 {
+	BWAPI::UnitData marineUnitData;
+	placeTerranMarine(marineUnitData, 1, 1);
+	marineUnitData.player = 0;
+	AKBot::UnitImpl marineUnitImpl(1, marineUnitData);
+
+	std::vector<BWAPI::Unit> ourCombatUnits;
+	ourCombatUnits.push_back(&marineUnitImpl);
+
+	BWAPI::UnitData zerglingUnitData;
+	placeZergZergeling(zerglingUnitData, 2, 2);
+	zerglingUnitData.player = 1;
+	AKBot::UnitImpl zerglingUnitImpl(2, zerglingUnitData);
+
+	UAlbertaBot::UnitInfo zergling(&zerglingUnitImpl);
+	std::vector<UAlbertaBot::UnitInfo> enemyCombatUnits;
+	enemyCombatUnits.push_back(zergling);
+	return estimator.isWinPredicted(ourCombatUnits, enemyCombatUnits, 100);
+}
+
+template<typename TCombatEstimator>
+void testCombatSimulation(
+	std::string name,
+	std::function<TCombatEstimator(std::shared_ptr<AKBot::GameImpl>)> estimatorFactory)
+{
+	clearTestCounter();
 	cout << "Testing combat simulation for combat estimator " << name << endl;
 
-	doTest("1 Zergling vs empty squad", testZergling1, estimator, TOTAL_SUCCESS);
-	doTest("1 Marine vs empty squad", testMarine1, estimator, TOTAL_SUCCESS);
+	doTest<TCombatEstimator>("1 Zergling vs empty squad", testZergling1, estimatorFactory, TOTAL_SUCCESS);
+	doTest<TCombatEstimator>("1 Marine vs empty squad", testMarine1, estimatorFactory, TOTAL_SUCCESS);
+	doTest<TCombatEstimator>("1 Marine vs 1 Zergling", testMarine2, estimatorFactory, TOTAL_FAILURE);
+
+	printStatistics();
+}
+
+FAPCombatEstimator getFap(std::shared_ptr<AKBot::GameImpl> gameImpl)
+{
+	return FAPCombatEstimator(configuration);
+}
+
+SparCraftCombatEstimator getSparCraft(std::shared_ptr<AKBot::GameImpl> gameImpl)
+{
+	auto game = gameImpl.get();
+	auto opponentView = std::make_shared<BWAPIOpponentView>(game);
+	auto logger = std::make_shared<AkBotTests::NullLogger>();
+	SparCraft::init();
+	auto path = std::experimental::filesystem::current_path();
+	auto sparcraftConfigFile = path.concat("/../../../../UAlbertaBot/bin/SparCraft_Config.txt").string();
+	SparCraft::AIParameters::Instance().parseFile(sparcraftConfigFile);
+	return SparCraftCombatEstimator(
+		opponentView,
+		logger,
+		sparcraftConfiguration,
+		configuration);
 }
 
 int main(int argc, char *argv[])
 {
-	BotMicroConfiguration configuration;
-	FAPCombatEstimator fap(configuration);
-	testCombatSimulation("FAP", fap);
+	testCombatSimulation<FAPCombatEstimator>("FAP", getFap);
+
+	testCombatSimulation<SparCraftCombatEstimator>("SparCraft", getSparCraft);
 
 	cout << "Press any key to continue." << endl;
 	cin.get();
