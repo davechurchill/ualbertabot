2b700b36dec7b50efa60fe6020dda3b9fd534da4
Andrii Kurdiumov <kant2002@gmail.com>
2018-04-24 19:57:20 +0600

    Create pluggable combat estimators

--- UAlbertaBot/Source/simulation/FAPCombatEstimator.h
+++ UAlbertaBot/Source/simulation/FAPCombatEstimator.h
@@ -5,10 +5,17 @@
 
 namespace AKBot
 {
+	typedef std::vector<UAlbertaBot::FastAPproximation::FAPUnit> FAPPlayerState;
+
 	class FAPCombatEstimator : public CombatEstimator
 	{
 	private:
 		UAlbertaBot::FastAPproximation fap;
+		std::pair<double, double> initialScores;
+		std::pair<double, double> finalScores;
+		std::function<double(const FAPCombatEstimator&)> _scoreFunction;
+		std::function<double(const FAPPlayerState&)> _playerScoreFunction;
+		
 		const BotMicroConfiguration& _microConfiguration;
 
 		void setCombatUnits(
@@ -16,12 +23,31 @@ namespace AKBot
 			std::vector<UAlbertaBot::UnitInfo> enemyCombatUnits,
 			int currentFrame);
 		double simulateCombat();
+		std::pair<double, double> calculateScore(std::function<double(const FAPPlayerState&)> playerScoreFunction);
 	public:
 		FAPCombatEstimator(
+			const BotMicroConfiguration& microConfiguration);
+		FAPCombatEstimator(
+			std::function<double(const FAPPlayerState&)> playerScoreFunction,
+			std::function<double(const FAPCombatEstimator&)> scoreFunction,
 			const BotMicroConfiguration& microConfiguration);
 		bool isWinPredicted(
 			const std::vector<BWAPI::Unit>& ourCombatUnits,
 			const std::vector<UAlbertaBot::UnitInfo>& enemyCombatUnits,
 			int currentFrame) override;
+
+		std::pair<FAPPlayerState*, FAPPlayerState*> getState() {
+			return fap.getState();
+		}
+		std::pair<double, double> playerScores() const { return finalScores; }
+		std::pair<int, int> playerScoresUnits() const { return fap.playerScoresUnits(); }
+		std::pair<int, int> playerScoresBuildings() const { return fap.playerScoresBuildings(); }
+
+		std::pair<double, double> getInitialScores() const { return initialScores; }
 	};
+	
+	double defaultScoreFunction(const FAPCombatEstimator& estimator);
+	double deltaScoreFunction(const FAPCombatEstimator& estimator);
+	double unitHealthScore(const FAPPlayerState& state);
+	double economicalCostScore(const FAPPlayerState& state);
 }
