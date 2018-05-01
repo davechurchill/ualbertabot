8c2f4ce6ec67998e2a9ad0901b9f0624964733a7
Andrii Kurdiumov <kant2002@gmail.com>
2018-05-01 15:38:45 +0600

    Pass game to bot creation API This is additional step to reduce dependency on static instance of BWAPI Game.

--- UAlbertaBot/Source/BotFactory.h
+++ UAlbertaBot/Source/BotFactory.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
+#include <BWAPI/Game.h>
 #include "BotPlayer.h"
 #include "BotConfiguration.h"
 
 namespace AKBot
 {
-	BotPlayer createBot(const std::string& mode, BotConfiguration& configuration, const std::string& configurationFilePath);
+	BotPlayer createBot(BWAPI::Game* game, const std::string& mode, BotConfiguration& configuration, const std::string& configurationFilePath);
 }
